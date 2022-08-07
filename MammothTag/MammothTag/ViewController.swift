34717b9a42e794230ae90414889c0ef160d6cfa3
Anwar Hajji <anwarhajji@Mac-Ouss.local>
2022-08-07 18:13:46 +0100

    start MammothTag project

---
+++ MammothTag/MammothTag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MammothTag
+//
+//  Created by Anwar Hajji on 07/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
